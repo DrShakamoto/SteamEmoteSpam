9dab0e8f8911e26dbbcf41688c5319b3aa6e2f85
DrShakamoto <baileysambhi@gmail.com>
2018-05-11 01:18:38 +0100

    Add files via upload

---
+++ emote spam.py
@@ -0,0 +1,22 @@
+import pyautogui
+
+print("This program only works on 1080p screens")
+print("Please set your steam chat window to snap to the right half of your screen")
+y_loops = input("Please enter how many rows of steam emotes you have")
+
+for i in range(y_loops):
+    x=i*23
+    y = 798 + x
+    for i in range(13):
+
+        z=24*i
+
+        pyautogui.moveTo(1826,1002)
+        pyautogui.click()
+
+        pyautogui.moveTo(1540, y)
+
+        pyautogui.moveRel(z, 0)
+        pyautogui.click()
+        
+    
